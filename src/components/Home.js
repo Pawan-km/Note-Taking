c422fa3b0ea6bd19bb28af885fd88cdf4af0d5e1
Pawan <pranaykunar.1461@gmail.com>
2021-12-21 11:53:03 +0530

    added code

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Notes from './Notes'
+
+
+function Home({ showAlert }) {
+
+    return (
+        <div>
+
+            <Notes showAlert={showAlert} />
+        </div>
+    )
+}
+
+export default Home
